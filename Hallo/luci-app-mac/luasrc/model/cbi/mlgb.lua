e95d5b8f28cb56c9c55852b3433fb2cf759f1abc
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-23 18:12:07 +0800

    Update mlgb.lua

--- Hallo/luci-app-mac/luasrc/model/cbi/mlgb.lua
+++ Hallo/luci-app-mac/luasrc/model/cbi/mlgb.lua
@@ -2,6 +2,6 @@ f = SimpleForm("qrcode", translate(""))
 f.reset = false
 f.submit = false
 
-f:section(SimpleSection).template  = "overview/overview_status"
+f:section(SimpleSection).template  = "overview_status"
 
 return f
