66b37ed9878235c4fc8e544622aa667fd12bb881
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-23 15:57:18 +0800

    Update brukamen_mac.lua

--- Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
+++ Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
@@ -1,13 +1,13 @@
 module("luci.controller.brukamen_mac", package.seeall)
 
 function index()
-    entry({"admin", "service"}, alias("admin", "service", "htm"), _("校园网"),1)
-    entry({"admin", "service", "htm"}, cbi("mlgb"), _("总览"), 1)
-    entry({"admin", "service", "brukamen_mac"}, cbi("brukamen_mac"), _("MAC克隆"), 3)
-    --entry({"admin", "service", "ua2f"}, cbi("ua2f"), "防检测配置", 4)
-    entry({"admin", "service", "Brukamen_WiFi"}, cbi("Brukamen_WiFi"), "WIFI设置", 2)
-    entry({"admin", "service", "lanipc"}, cbi("lanip"), _("修改IP"), 5)
-    entry({"admin", "service", "autoreboot"}, cbi("autoreboot"), _("定时重启"), 100)
-    --entry({"admin", "service", "CQR"}, cbi("donation"), _("支持我们"), 101)
+    entry({"admin", "school"}, alias("admin", "school", "htm"), _("校园网"),1)
+    entry({"admin", "school", "htm"}, cbi("mlgb"), _("总览"), 1)
+    entry({"admin", "school", "brukamen_mac"}, cbi("brukamen_mac"), _("MAC克隆"), 3)
+    --entry({"admin", "school", "ua2f"}, cbi("ua2f"), "防检测配置", 4)
+    entry({"admin", "school", "Brukamen_WiFi"}, cbi("Brukamen_WiFi"), "WIFI设置", 2)
+    entry({"admin", "school", "lanipc"}, cbi("lanip"), _("修改IP"), 5)
+    entry({"admin", "school", "autoreboot"}, cbi("autoreboot"), _("定时重启"), 100)
+    --entry({"admin", "school", "CQR"}, cbi("donation"), _("支持我们"), 101)
 end
 
