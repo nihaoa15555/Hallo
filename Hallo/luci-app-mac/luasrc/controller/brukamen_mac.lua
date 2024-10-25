d9a8df9535f9c616c909da2001a25c5578fa44a2
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-25 15:01:13 +0800

    Update brukamen_mac.lua

--- Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
+++ Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
@@ -1,5 +1,5 @@
 module("luci.controller.brukamen_mac", package.seeall)
 
 function index()
-    entry({"admin", "services", "brukamen_mac"}, cbi("brukamen_mac"), _("MAC克隆"), 94)
+    entry({"admin", "services", "brukamen_mac"}, cbi("brukamen_mac"), _("MAC克隆"), 99)
 end
