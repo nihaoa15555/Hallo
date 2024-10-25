8c03b3b89294713e7f380daf07c34f0ffd0eae3f
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-25 10:01:45 +0800

    Update brukamen_mac.lua

--- Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
+++ Hallo/luci-app-mac/luasrc/controller/brukamen_mac.lua
@@ -2,7 +2,7 @@ module("luci.controller.brukamen_mac", package.seeall)
 
 function index()
     entry({"admin", "school"}, alias("admin", "school", "htm"), _("校园网"),1)
-    entry({"admin", "school", "htm"}, cbi("mlgb"), _("总览"), 1)
+    entry({"admin", "school", "htm"}, cbi("mlgb"), _("路由情况"), 1)
     entry({"admin", "school", "brukamen_mac"}, cbi("brukamen_mac"), _("MAC克隆"), 3)
     --entry({"admin", "school", "ua2f"}, cbi("ua2f"), "防检测配置", 4)
     entry({"admin", "school", "Brukamen_WiFi"}, cbi("Brukamen_WiFi"), "WIFI设置", 2)
