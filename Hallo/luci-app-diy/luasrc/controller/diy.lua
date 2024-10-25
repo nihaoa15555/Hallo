38b7af55326cfdd95552276cdb039a6dae33e651
nihaoa15555 <160318809+nihaoa15555@users.noreply.github.com>
2024-10-25 15:21:33 +0800

    Update diy.lua

--- Hallo/luci-app-diy/luasrc/controller/diy.lua
+++ Hallo/luci-app-diy/luasrc/controller/diy.lua
@@ -1,7 +1,7 @@
 module("luci.controller.diy", package.seeall)
 
 function index()
-    entry({"admin", "school"}, alias("admin", "school", "htm"), _("校园网"),1)
+    entry({"admin", "school"}, alias("admin", "school", "htm"), _("快速设置"),1)
     entry({"admin", "school", "htm"}, cbi("mlgb"), _("路由情况"), 1)
     entry({"admin", "school", "WiFi"}, cbi("WiFi"), "WIFI设置", 2)
     entry({"admin", "school", "lanipc"}, cbi("lanip"), _("修改IP"), 5)
