9f9bff130fe15fedb9399841d3632bdadc917389
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-26 21:47:14 +0800

    Update autoua2f.lua

--- Hallo/luci-app-ua2f/model/cbi/autoua2f.lua
+++ Hallo/luci-app-ua2f/model/cbi/autoua2f.lua
@@ -11,29 +11,29 @@ e.addremove = false
 e.anonymous = true
 
 o1 = e:option(Value, "enabled", translate("启用/开机自启"))
-o1:value("0", "启用")
-o1:value("1", "禁用")
+o1:value("1", "启用")
+o1:value("0", "禁用")
 o1.default = "0"
 
 o2 = e:option(Value, "handle_fw", translate("自动配置防火墙"), translate("是否自动添加防火墙规则"))
-o2:value("0", "启用")
-o2:value("1", "禁用")
-o2.default = "0"
+o2:value("1", "启用")
+o2:value("0", "禁用")
+o2.default = "1"
 
 
 o3 = e:option(Value, "handle_intranet", translate("处理内网流量"), translate("是否处理内网流量，如果你的路由器是在内网中，且你想要处理内网中的流量，那么请启用这一选项"))
-o3:value("0", "启用")
-o3:value("1", "禁用")
-o3.default = "0"
+o3:value("1", "启用")
+o3:value("0", "禁用")
+o3.default = "1"
 
 o4 = e:option(Value, "handle_tls", translate("处理443端口流量"), translate("通常来说，流经 443 端口的流量是加密的，因此无需处理"))
-o4:value("0", "启用")
-o4:value("1", "禁用")
+o4:value("1", "启用")
+o4:value("0", "禁用")
 o4.default = "0"
 
 o5 = e:option(Value, "handle_mmtls", translate("处理微信流量"), translate("微信的流量通常是加密的，因此无需处理，这一规则在启用 nftables 时无效"))
-o5:value("0", "启用")
-o5:value("1", "禁用")
+o5:value("1", "启用")
+o5:value("0", "禁用")
 o5.default = "0"
 
 o6 = e:option(Value, "Custom_UA", translate("自定义用户代理"), translate("自定义用户代理字符串，长度不足则填充空格，过长则截取与原来长度相同的子串"))
