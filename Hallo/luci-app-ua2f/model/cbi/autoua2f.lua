579ea41ea88ab335e9b555bf54dffadce29885b2
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-27 17:43:32 +0800

    Update autoua2f.lua

--- Hallo/luci-app-ua2f/model/cbi/autoua2f.lua
+++ Hallo/luci-app-ua2f/model/cbi/autoua2f.lua
@@ -26,17 +26,17 @@ o3:value("1", "启用")
 o3:value("0", "禁用")
 o3.default = "1"
 
-o4 = e:option(ListValue, "handle_tls", translate("处理443端口流量"), translate("通常来说，流经 443 端口的流量是加密的，因此无需处理"))
+o4 = e:option(ListValue, "handle_tls", translate("处理443端口流量"), translate(" 443 端口的流量是加密的，因此无需处理"))
 o4:value("1", "启用")
 o4:value("0", "禁用")
 o4.default = "0"
 
-o5 = e:option(ListValue, "handle_mmtls", translate("处理微信流量"), translate("微信的流量通常是加密的，因此无需处理，这一规则在启用 nftables 时无效"))
+o5 = e:option(ListValue, "handle_mmtls", translate("处理微信流量"), translate("微信的流量通常是加密的，因此无需处理"))
 o5:value("1", "启用")
 o5:value("0", "禁用")
 o5.default = "0"
 
-o6 = e:option(Value, "Custom_UA", translate("自定义用户代理"), translate("自定义用户代理字符串，长度不足则填充空格，过长则截取与原来长度相同的子串"))
+o6 = e:option(Value, "Custom_UA", translate("自定义用户代理"), translate("自定义UA设置"))
 o6.default = "Mozilla/5.0 (Window NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/555.66"
 
 m.on_commit = function(self)
