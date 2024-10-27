f1044221759b778c218cb86814c31ede8bc3151b
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-10-27 17:42:48 +0800

    Update school.lua

--- Hallo/luci-app-school/luasrc/model/cbi/school/school.lua
+++ Hallo/luci-app-school/luasrc/model/cbi/school/school.lua
@@ -8,31 +8,31 @@ s = m:section(TypedSection, "school")
 s.addremove = false
 s.anonymous = true
 
-IPID = s:option(ListValue, "IPID", translate("iptables IPID 防检测"))
+IPID = s:option(ListValue, "IPID", translate(" IPID 防检测 "))
 IPID:value("1", "启用")
 IPID:value("0", "禁用")
 IPID.default = "0"
 IPID.description = translate("iptables IPID 防检测")
 
-IUA = s:option(ListValue, "IUA", translate("修改 HTTP-Headr 防检测"))
+IUA = s:option(ListValue, "IUA", translate(" HTTP-Headr 防检测 "))
 IUA:value("1", "启用")
 IUA:value("0", "禁用")
 IUA.default = "0"
 IUA.description = translate("修改 HTTP-Headr 防检测")
 
-INTP = s:option(ListValue, "INTP", translate("修正 NTP 防检测"))
+INTP = s:option(ListValue, "INTP", translate("修正 NTP 防检测 "))
 INTP:value("1", "启用")
 INTP:value("0", "禁用")
 INTP.default = "0"
 INTP.description = translate("修正 NTP 防检测")
 
-ITTL = s:option(ListValue, "ITTL", translate("iptables 修改 TTL 防检测"))
+ITTL = s:option(ListValue, "ITTL", translate(" 修改 TTL "))
 ITTL:value("1", "启用")
 ITTL:value("0", "禁用")
 ITTL.default = "0"
 ITTL.description = translate("iptables 修改 TTL 防检测")
 
-IACFlash = s:option(ListValue, "IACFlash", translate("iptables 拒绝 AC 进行 Flash 检测"))
+IACFlash = s:option(ListValue, "IACFlash", translate(" 拒绝 AC 进行 Flash 检测 "))
 IACFlash:value("1", "启用")
 IACFlash:value("0", "禁用")
 IACFlash.default = "0"
