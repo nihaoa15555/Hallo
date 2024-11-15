f68d57ac39c112b64dee106908baa15ca53d0815
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-15 19:36:28 +0800

    Update MAC_clone.lua

--- Hallo/luci-app-mac/luasrc/controller/MAC_clone.lua
+++ Hallo/luci-app-mac/luasrc/controller/MAC_clone.lua
@@ -1,6 +1,7 @@
 module("luci.controller.MAC_clone", package.seeall)
 
 function index()
-	entry({"admin", "services", "MAC_clone"},alias("admin", "services", "MAC_clone","commonly"), _("MAC更改"))
-	entry({"admin", "services", "MAC_clone", "commonly"},cbi("MAC_clone"), _("MAC设置"), 10).leaf = true
+	entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测设置"),2)
+	entry({"admin", "school", "MAC_clone"},alias("admin", "school", "MAC_clone","commonly"), _("MAC克隆"))
+	entry({"admin", "school", "MAC_clone", "commonly"},cbi("MAC_clone"), _("MAC设置"), 10).leaf = true
 end
