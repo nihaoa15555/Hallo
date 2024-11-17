1310322b62df1ffa6d2277bef1eaafd236758330
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-17 09:10:45 +0800

    Update WiFi.lua

--- Hallo/luci-app-diy/luasrc/model/cbi/WiFi.lua
+++ Hallo/luci-app-diy/luasrc/model/cbi/WiFi.lua
@@ -1,6 +1,6 @@
 m = Map("wireless", "Wi-Fi设置", "配置Wi-Fi设置")
 
-s = m:section(TypedSection, "wifi-iface", "修改为不同名称即可分为两个wifi信号（一般第一个为2.4G 第二个为5G）")
+s = m:section(TypedSection, "wifi-iface", "修改为不同名称即可分为两个wifi信号")
 s.anonymous = true
 
 o = s:option(Value, "ssid", "Wi-Fi名称")
@@ -13,14 +13,14 @@ o.default = "0"
 
 
 o = s:option(ListValue, "encryption", "加密类型")
-o:value("psk", "WPA/WPA2-PSK")
+o:value("psk2", "WPA2-PSK")
 o:value("none", "无线不加密")
 o.default = "none"
 
 o = s:option(Value, "key", "密码")
 o.datatype = "wpakey"
 o.password = true
-o:depends("encryption", "psk")
+o:depends("encryption", "psk2")
 o.validate = function(self, value)
     if #value < 8 then
         return nil, "密码至少需要8个字符"
