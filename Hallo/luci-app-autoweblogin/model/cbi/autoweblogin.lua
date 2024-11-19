45509de894a8fe9090c17592de012e231adec14a
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-19 19:16:12 +0800

    Update autoweblogin.lua

--- Hallo/luci-app-autoweblogin/model/cbi/autoweblogin.lua
+++ Hallo/luci-app-autoweblogin/model/cbi/autoweblogin.lua
@@ -12,21 +12,15 @@ o1:value("1", "启用")
 o1:value("0", "禁用")
 o1.default = "0"
 
-o = e:option(ListValue, "mode", translate("模式选择"))
-o:value("1", "韶关学院")
-o:value("2", "沈阳科技")
-o.default = "1"
+o2 = e:option(ListValue, "mode", translate("模式选择"))
+o2:value("1", "韶关学院")
+o2:value("2", "沈阳科技")
+o2.default = "1"
 
-o2 = e:option(Value, "user_account", translate("账号"))
+o3 = e:option(Value, "user_account", translate("账号"))
 
-o3 = e:option(Value, "user_password", translate("密码"))
-o3.password = true
-
-o = e:option(ListValue, "interface", translate("选择获取IP的接口"), translate("根据实际情况选择外网接口，一般为eth1或wan"))
-for t, e in ipairs(i.net.devices()) do
-    if e ~= "lo" then o:value(e) end
-end
-o.rmempty = false
+o4 = e:option(Value, "user_password", translate("密码"))
+o4.password = true
 
 o5 = e:option(Value, "time", translate("网络检测间隔"), translate("以秒为单位"))
 
