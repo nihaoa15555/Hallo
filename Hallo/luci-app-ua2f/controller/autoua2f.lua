5bd3c3de04497498252692856740332fc7961d48
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-20 09:44:39 +0800

    Add files via upload

---
+++ Hallo/luci-app-ua2f/controller/autoua2f.lua
@@ -0,0 +1,17 @@
+module("luci.controller.autoua2f", package.seeall)
+
+function index()
+    entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测设置"),2)
+    entry({"admin", "school", "autoua2f"}, cbi("autoua2f"), _("防检测设置"), 94)
+    entry({"admin", "school", "autoua2f", "status"}, call("act_status")).leaf = true
+
+end
+
+function act_status()
+	local e = {}
+	e.running = luci.sys.call("pgrep ua2f >/dev/null") == 0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
+end
+
+
