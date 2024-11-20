546bacf083968a39cace0cf13195066c49860594
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-20 11:38:46 +0800

    Update autoua2f.lua

--- Hallo/luci-app-ua2f/controller/autoua2f.lua
+++ Hallo/luci-app-ua2f/controller/autoua2f.lua
@@ -2,16 +2,7 @@ module("luci.controller.autoua2f", package.seeall)
 
 function index()
     entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测配置"),2)
-    entry({"admin", "school", "autoua2f"}, cbi("autoua2f"), _("防检测"), 94)
-    entry({"admin", "school", "autoua2f", "status"}, call("act_status")).leaf = true
-
-end
-
-function act_status()
-	local e = {}
-	e.running = luci.sys.call("pgrep ua2f >/dev/null") == 0
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(e)
+    entry({"admin", "school", "autoua2f"}, cbi("autoua2f"), _("防检测"), 94).leaf = true
 end
 
 
