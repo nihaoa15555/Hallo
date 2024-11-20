a34c92504eef68f24655d932847c613d1a5155be
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2024-11-20 09:42:26 +0800

    Delete Hallo/luci-app-defense directory

--- Hallo/luci-app-defense/controller/defense.lua
+++
@@ -1,8 +0,0 @@
-module("luci.controller.defense", package.seeall)
-
-function index()
-    entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测"),2)
-    entry({"admin", "school", "defense"}, cbi("defense"), _("防检测设置"), 94).leaf = true
-end
-
-
