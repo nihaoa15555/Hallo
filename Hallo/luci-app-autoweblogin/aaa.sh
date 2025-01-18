e9fc6f9257713bd18301f24837de88d7726f37cc
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2025-01-18 22:12:19 +0800

    Update aaa.sh

--- Hallo/luci-app-autoweblogin/aaa.sh
+++ Hallo/luci-app-autoweblogin/aaa.sh
@@ -11,7 +11,7 @@ Nanoseconds=$(date -u +%N)
 Milliseconds=$((Seconds * 1000 + Nanoseconds / 1000000))
 response_file="/tmp/response.txt"
 
-MODE1() {
+portal1() {
     rm "$response_file"
     echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "http://172.16.253.121/quickauth.do?userid=$USERID&passwd=$PASSWD&wlanuserip=$WLANIP&wlanacname=NFV-BASE-SGYD2&wlanacIp=172.16.253.114&ssid=&vlan=1116&mac=$MAC&version=0&portalpageid=2&timestamp=$Milliseconds&portaltype=0&hostname=HuaWei&bindCtrlId=&validateType=0&bindOperatorType=2&sendFttrNotice=0" \
@@ -20,7 +20,7 @@ MODE1() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 服务器返回：$response" >> "$log_file"
 }
 
-MODE2() {
+portal2() {
     rm "$response_file"
     echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "http://172.16.13.10:6060/quickauth.do?userid=$USERID&passwd=$PASSWD&wlanuserip=$WLANIP&wlanacname=NFV-BASE&wlanacIp=172.16.13.11&ssid=&vlan=24012633&mac=$MAC&version=0&portalpageid=2&validateCode=&timestamp=$Milliseconds&portaltype=0&hostname=HuaWei&bindCtrlId=&validateType=0&bindOperatorType=2" \
@@ -29,7 +29,7 @@ MODE2() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 服务器返回：$response" >> "$log_file"
 }
 
-MODE3() {
+portal3() {
     rm "$response_file"
     echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "xxxxx" \
@@ -67,13 +67,13 @@ while true; do
 
             case "$MODE" in
                 MODE1)
-                    MODE1
+                    portal1
                     ;;
                 MODE2)
-                    MODE2
+                    portal2
                     ;;
                 MODE3)
-                    MODE3
+                    portal3
                     ;;
             esac
             
