797638eec83b528ef821ba981639cf6235216d3a
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2025-01-18 22:11:35 +0800

    Update aaa.sh

--- Hallo/luci-app-autoweblogin/aaa.sh
+++ Hallo/luci-app-autoweblogin/aaa.sh
@@ -6,51 +6,36 @@ PASSWD="$(uci get autoweblogin.config.user_password)"
 TIME="$(uci get autoweblogin.config.time)"
 WLANIP="$(ifconfig eth1 | grep 'inet addr:' | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n 1)"
 MAC="$(ifconfig eth1 | grep -oE '([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}')"   
+Seconds=$(date -u +%s)
+Nanoseconds=$(date -u +%N)
+Milliseconds=$((Seconds * 1000 + Nanoseconds / 1000000))
 response_file="/tmp/response.txt"
 
 MODE1() {
     rm "$response_file"
-    echo "请求参数——账号：$USERID——密码：$PASSWD——IP地址：$WLANIP——MAC地址：$MAC" >> "$log_file"
-    
-    Seconds=$(date -u +%s)
-    Nanoseconds=$(date -u +%N)
-    Milliseconds=$((Seconds * 1000 + Nanoseconds / 1000000))
-
+    echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "http://172.16.253.121/quickauth.do?userid=$USERID&passwd=$PASSWD&wlanuserip=$WLANIP&wlanacname=NFV-BASE-SGYD2&wlanacIp=172.16.253.114&ssid=&vlan=1116&mac=$MAC&version=0&portalpageid=2&timestamp=$Milliseconds&portaltype=0&hostname=HuaWei&bindCtrlId=&validateType=0&bindOperatorType=2&sendFttrNotice=0" \
 	  -o "$response_file"
     response=$(cat "$response_file")
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 服务器返回：$response" >> "$log_file"
-
 }
 
 MODE2() {
     rm "$response_file"
-    echo "请求参数——账号：$USERID——密码：$PASSWD——IP地址：$WLANIP——MAC地址：$MAC" >> "$log_file"
-
-    Seconds=$(date -u +%s)
-    Nanoseconds=$(date -u +%N)
-    Milliseconds=$((Seconds * 1000 + Nanoseconds / 1000000))
-    
+    echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "http://172.16.13.10:6060/quickauth.do?userid=$USERID&passwd=$PASSWD&wlanuserip=$WLANIP&wlanacname=NFV-BASE&wlanacIp=172.16.13.11&ssid=&vlan=24012633&mac=$MAC&version=0&portalpageid=2&validateCode=&timestamp=$Milliseconds&portaltype=0&hostname=HuaWei&bindCtrlId=&validateType=0&bindOperatorType=2" \
 	  -o "$response_file"
     response=$(cat "$response_file")
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 服务器返回：$response" >> "$log_file"
-
 }
 
 MODE3() {
     rm "$response_file"
-    echo "请求参数——账号：$USERID——密码：$PASSWD——IP地址：$WLANIP——MAC地址：$MAC" >> "$log_file"
-
-    Seconds=$(date -u +%s)
-    Nanoseconds=$(date -u +%N)
-    Milliseconds=$((Seconds * 1000 + Nanoseconds / 1000000))
-
+    echo "请求参数-账号：$USERID  密码：$PASSWD  IP地址：$WLANIP  MAC地址：$MAC" >> "$log_file"
 	curl "xxxxx" \
 	  -o "$response_file"
     response=$(cat "$response_file")
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] 服务器返回：$response" >> "$log_file"
-
 }
 
 
