53aee3533c2e108e43b5197b69d920ebf9ee6376
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2025-04-05 14:49:02 +0800

    Update autoweblogin.lua

--- Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
+++ Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
@@ -2,7 +2,7 @@ module("luci.controller.autoweblogin", package.seeall)
 
 function index()
     entry({"admin", "auto"}, alias("admin", "auto", "htm"), _("网页认证"),3)
-    entry({"admin", "auto", "autoweblogin"}, alias("admin", "auto", "autoweblogin", "post"), _("校园网认证"), 99).index = true
+    entry({"admin", "auto", "autoweblogin"}, alias("admin", "auto", "autoweblogin", "post"), _("校园网登入"), 99).index = true
     entry({"admin", "auto", "autoweblogin", "post"}, cbi("autoweblogin"), _("认证设置"), 1)
     entry({"admin", "auto", "autoweblogin", "log"}, cbi("autoweblogin_log"), _("认证日志"), 2)
     entry({"admin", "auto", "autoweblogin", "status"}, call("act_status")).leaf = true
