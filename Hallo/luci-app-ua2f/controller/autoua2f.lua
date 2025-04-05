b9a61ae5e3c2b5455fe622e407901bf1bebe2f6c
SlepBB <160318809+nihaoa15555@users.noreply.github.com>
2025-04-05 14:51:14 +0800

    Update autoua2f.lua

--- Hallo/luci-app-ua2f/controller/autoua2f.lua
+++ Hallo/luci-app-ua2f/controller/autoua2f.lua
@@ -1,7 +1,7 @@
 module("luci.controller.autoua2f", package.seeall)
 
 function index()
-    entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测配置"),2)
+    entry({"admin", "school"}, alias("admin", "school", "htm"), _("防检测"),2)
     entry({"admin", "school", "autoua2f"}, cbi("autoua2f"), _("防检测"), 94).leaf = true
 end
 
