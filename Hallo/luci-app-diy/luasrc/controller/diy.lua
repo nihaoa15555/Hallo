5917f9b8252787c9295f78f0f4e46d7c25d10f21
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-11 21:11:37 +0800

    Update diy.lua

--- Hallo/luci-app-diy/luasrc/controller/diy.lua
+++ Hallo/luci-app-diy/luasrc/controller/diy.lua
@@ -1,10 +1,11 @@
 module("luci.controller.diy", package.seeall)
 
 function index()
-    entry({"admin", "diy"}, alias("admin", "diy", "htm"), _("WIFI"),1)
+    entry({"admin", "diy"}, alias("admin", "diy", "htm"), _("总览"),1)
     entry({"admin", "diy", "htm"}, cbi("mlgb"), _("路由状态"), 1)
     entry({"admin", "diy", "WiFi"}, cbi("WiFi"), "WIFI设置", 2)
     entry({"admin", "diy", "lanipc"}, cbi("lanip"), _("修改IP"), 5)
     --entry({"admin", "diy", "autoreboot"}, cbi("autoreboot"), _("定时重启"), 100)
 end
+
 
