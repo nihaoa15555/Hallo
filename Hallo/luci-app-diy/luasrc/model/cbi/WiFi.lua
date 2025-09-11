fb2d13b2a152f57303ea29d56e3dfc83a2aa1457
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-11 21:06:57 +0800

    Update WiFi.lua

--- Hallo/luci-app-diy/luasrc/model/cbi/WiFi.lua
+++ Hallo/luci-app-diy/luasrc/model/cbi/WiFi.lua
@@ -1,6 +1,6 @@
-m = Map("wireless", "Wi-Fi设置", "配置Wi-Fi设置")
+m = Map("wireless", "Wi-Fi设置" )
 
-s = m:section(TypedSection, "wifi-iface", "修改为不同名称即可分为两个wifi信号")
+s = m:section(TypedSection, "wifi-iface", "修改为不同名称即可分为两个Wi-Fi信号")
 s.anonymous = true
 
 o = s:option(Value, "ssid", "Wi-Fi名称")
@@ -30,4 +30,5 @@ o.validate = function(self, value)
 end
 
 return m
+
 
