e9db0553b8259c7d3f90a3df8f8aa2a8b1e8d8d5
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-11 21:12:12 +0800

    Update autoweblogin.lua

--- Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
+++ Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
@@ -2,7 +2,7 @@ module("luci.controller.autoweblogin", package.seeall)
 
 function index()
     entry({"admin", "auto"}, alias("admin", "auto", "htm"), _("网页认证"),3)
-    entry({"admin", "auto", "autoweblogin"}, alias("admin", "auto", "autoweblogin", "post"), _("校园网登入"), 99).index = true
+    entry({"admin", "auto", "autoweblogin"}, alias("admin", "auto", "autoweblogin", "post"), _("校园网自动登入"), 99).index = true
     entry({"admin", "auto", "autoweblogin", "post"}, cbi("autoweblogin"), _("认证设置"), 1)
     entry({"admin", "auto", "autoweblogin", "log"}, cbi("autoweblogin_log"), _("认证日志"), 2)
     entry({"admin", "auto", "autoweblogin", "status"}, call("act_status")).leaf = true
@@ -15,4 +15,5 @@ function act_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
+
 
