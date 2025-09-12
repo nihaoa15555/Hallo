74cffad7568c30b83e9c271050711d3c515eb0b3
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-12 17:44:40 +0800

    Update MAC_clone.lua

--- Hallo/luci-app-mac/controller/MAC_clone.lua
+++ Hallo/luci-app-mac/controller/MAC_clone.lua
@@ -1,6 +1,5 @@
 module("luci.controller.MAC_clone", package.seeall)
 
 function index()
-	entry({"admin", "school", "MAC_clone"},alias("admin", "school", "MAC_clone","commonly"), _("MAC克隆"))
-	entry({"admin", "school", "MAC_clone", "commonly"},cbi("MAC_clone"), _("MAC设置"), 10).leaf = true
+    entry({"admin", "school", "MAC_clone"}, cbi("MAC_clone"), _("MAC克隆"), 10).leaf = true
 end
