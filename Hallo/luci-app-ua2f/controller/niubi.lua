8c25f1a6535f1b7d1c503768c866d244b1e4250c
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-14 20:41:16 +0800

    Update niubi.lua

--- Hallo/luci-app-ua2f/controller/niubi.lua
+++ Hallo/luci-app-ua2f/controller/niubi.lua
@@ -2,8 +2,9 @@ module("luci.controller.niubi", package.seeall)
 
 function index()
     entry({"admin", "school"}, alias("admin", "school", "htm"), _("校园网"),2)
-    entry({"admin", "school", "niubi"}, cbi("niubi"), _("防检测"), 94).leaf = true
+    entry({"admin", "school", "niubi"}, cbi("niubi"), _("防检测"), 40).leaf = true
 end
+
 
 
 
