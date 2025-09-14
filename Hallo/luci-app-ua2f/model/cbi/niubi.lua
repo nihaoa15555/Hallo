b647fc2dbbfe9c1046f87e02e2435c074625b5e2
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-14 20:35:38 +0800

    Update and rename autoua2f.lua to niubi.lua

--- Hallo/luci-app-ua2f/model/cbi/niubi.lua
+++ Hallo/luci-app-ua2f/model/cbi/niubi.lua
@@ -1,8 +1,8 @@
-m = Map("autoua2f", translate("防检测"))
+m = Map("niubi", translate("防检测"))
 
-m:section(SimpleSection).template = "ua2f/ua2f_A"
+m:section(SimpleSection).template = "niubi/niubi_A"
 
-e = m:section(TypedSection, "autoua2f", translate("设置"))
+e = m:section(TypedSection, "niubi", translate("设置"))
 e.addremove = false
 e.anonymous = true
 
@@ -11,11 +11,11 @@ o1:value("1", "启用")
 o1:value("0", "禁用")
 o1.default = "0"
 
-o2 = e:option(Value, "Custom_UA", translate("自定义用户代理"), translate("自定义UA"))
+o2 = e:option(Value, "Custom_UA", translate("UA"), translate("自定义UA"))
 o2.default = "Mozilla/5.0 (Window NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/555.66"
 
 m.on_commit = function(self)
-    luci.sys.call("/etc/init.d/autoua2f start")
+    luci.sys.call("/etc/init.d/niubi start")
 end
 
 return m
