703054cea4d0d27912adbd83020f22526fb1718b
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-14 21:05:14 +0800

    Update MAC.lua

--- Hallo/luci-app-mac/controller/MAC.lua
+++ Hallo/luci-app-mac/controller/MAC.lua
@@ -1,5 +1,5 @@
 module("luci.controller.MAC", package.seeall)
 
 function index()
-    entry({"admin", "school", "MAC"}, cbi("MAC"), _("MAC克隆"), 10).leaf = true
+    entry({"admin", "school", "MAC"}, cbi("MAC"), _("MAC克隆"), 90).leaf = true
 end
