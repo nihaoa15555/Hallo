7572b7d91673d8be77dde7248394e17baa85d10c
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-09-14 23:20:34 +0800

    Add files via upload

---
+++ Hallo/luci-app-ua2f/model/cbi/autoua2f.lua
@@ -0,0 +1,21 @@
+m = Map("autoua2f", translate("防检测"))
+
+m:section(SimpleSection).template = "ua2f/ua2f_A"
+
+e = m:section(TypedSection, "autoua2f", translate("设置"))
+e.addremove = false
+e.anonymous = true
+
+o1 = e:option(ListValue, "enabled", translate("启用/开机自启"))
+o1:value("1", "启用")
+o1:value("0", "禁用")
+o1.default = "0"
+
+o2 = e:option(Value, "Custom_UA", translate("自定义用户代理"), translate("自定义UA"))
+o2.default = "Mozilla/5.0 (Window NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/555.66"
+
+m.on_commit = function(self)
+    luci.sys.call("/etc/init.d/autoua2f start")
+end
+
+return m
