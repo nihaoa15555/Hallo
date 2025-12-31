8d4c9fdf694fa00e83108445c642fd00548e1c51
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2025-12-31 15:59:23 +0800

    Update autoweblogin.lua

--- Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
+++ Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
@@ -9,12 +9,6 @@ function index()
 
 end
 
-function act_status()
-	local e = {}
-	e.running = luci.sys.call("ps | grep autoweblogin | grep -v grep >/dev/null") == 0
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(e)
-end
 
 
 
