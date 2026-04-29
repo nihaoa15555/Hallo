493cdf76ccfb273537fb2e4af0f06fc5ffe80933
ENVY <160318809+nihaoa15555@users.noreply.github.com>
2026-04-29 19:23:03 +0800

    Update autoweblogin.lua

--- Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
+++ Hallo/luci-app-autoweblogin/controller/autoweblogin.lua
@@ -1,9 +1,10 @@
 module("luci.controller.autoweblogin", package.seeall)
 
 function index()
-    entry({"admin", "auto"}, cbi("autoweblogin"), _("网页认证"), 3).index = true
-    entry({"admin", "auto", "log"}, cbi("autoweblogin_log"), _("认证日志"), 1)
-    entry({"admin", "auto", "status"}, call("act_status")).leaf = true
+   entry({"admin", "auto"}, alias("admin", "auto", "htm"), _("网页认证"),3)
+    entry({"admin", "auto", "autoweblogin"}, alias("admin", "auto", "autoweblogin", "post"), _("自动登入"), 99).index = true
+    entry({"admin", "auto", "autoweblogin", "post"}, cbi("autoweblogin"), _("认证设置"), 1)
+    entry({"admin", "auto", "autoweblogin", "log"}, cbi("autoweblogin_log"), _("认证日志"), 2)
     entry({"admin", "auto", "autoweblogin", "status"}, call("act_status")).leaf = true
 
 end
